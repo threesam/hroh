71fe05b4a070e04802c6ffe26d8aa21397e6a019
threesam <salvatoredangelo@protonmail.com>
2020-10-23 14:36:18 -0400

    added blog route

---
+++ web/src/routes/blog/index.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+  import client from '../../sanityClient'
+  import BlockContent from '@movingbrands/svelte-portable-text'
+  import serializers from '../../components/serializers'
+  export function preload({ params }) {
+    const filter = `*[_type == 'post']`
+    const projection = `{
+          ...,
+          "body": body[].children[],
+          "slug": slug.current, 
+          publishedAt, 
+          title,
+          "author": authors[0].author->name,
+          "image": mainImage.asset->url,
+          "alt": mainImage.alt,
+        }`
+    const query = filter + projection
+    return client
+      .fetch(query)
+      .then((posts) => {
+        return { posts }
+      })
+      .catch((err) => this.error(500, err))
+  }
+</script>
+
+<script>
+    export let posts
+    console.log(posts[0].body)
+</script>
+
+<h1>Recent Posts</h1>
+
+{#each posts as {title, author, image, alt, body}}
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    <BlockContent {body} {serializers} />
+{/each}
