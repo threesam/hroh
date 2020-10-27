ae2ac5455a4fad340971f4b2302ba979a8f5f7c9
threesam <salvatoredangelo@protonmail.com>
2020-10-27 07:22:29 -0400

    added prefetch to about and contact routes

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -5,6 +5,6 @@
 <nav id="navbar">
 	<a href="." class='{segment === undefined ? "selected-tab" : ""} nav-link'>Home</a>
 	<a href="about" class="{segment === "about" ? "selected-tab" : ""} nav-link">About</a>
-	<a href="press" class="{segment === "press" ? "selected-tab" : ""} nav-link">Press</a>
-	<a href="contact" class="{segment === "contact" ? "selected-tab" : ""} nav-link">Contact</a>
+	<a rel="prefetch" href="press" class="{segment === "press" ? "selected-tab" : ""} nav-link">Press</a>
+	<a rel="prefetch" href="contact" class="{segment === "contact" ? "selected-tab" : ""} nav-link">Contact</a>
 </nav>
