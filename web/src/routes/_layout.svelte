901daaf2aafb97bfc72a4c89939707399ebfaa43
threesam <salvatoredangelo@protonmail.com>
2020-11-07 13:52:13 -0500

    removed test test

--- web/src/routes/_layout.svelte
+++ web/src/routes/_layout.svelte
@@ -1,14 +1,8 @@
   <script>
-	import {onMount, tick} from 'svelte'
 	import Nav from '../components/Nav.svelte';
 	import Footer from '../components/Footer.svelte';
 
 	export let segment;
-
-	let testId = 'loading'
-	onMount(()=> {
-		tick().then(() => testId = 'loaded')
-	})
 </script>
 
 <style>
