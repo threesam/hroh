d2add73741394a3879ed06239c4c86d2cd72e232
threesam <salvatoredangelo@protonmail.com>
2020-11-08 07:37:13 -0500

    factored praise>card out of global css into praiseCard

--- web/src/routes/_PraiseCard.svelte
+++ web/src/routes/_PraiseCard.svelte
@@ -7,6 +7,10 @@
 </script>
 			
 <style>
+    div {
+        padding: 0.5rem 0;
+        margin: 0.5rem 0;
+    }
 	span {
         font-size: 1rem;
         color: var(--emphasized-text);
@@ -15,7 +19,7 @@
     a {
         display: block;
         width: max-content;
-        margin: 0.75rem 0;
+        margin-top: 0.75rem;
         text-align: left;
         text-transform: lowercase;
         font-size: 0.6rem;
@@ -28,7 +32,9 @@
 
     p {
         font-size: 0.75rem;
+        margin-top: 0.5rem;
         line-height: 1.5;
+        opacity: 0.69;
     }
 </style>
 
