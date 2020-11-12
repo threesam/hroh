2f25ac578c2146fa2f107cd99bf6176b939bc93c
threesam <salvatoredangelo@protonmail.com>
2020-11-12 15:33:17 -0500

    extracted button component, applied subscribe-link global styles and swapped out main call to action

--- web/src/routes/_Gumroad.svelte
+++ web/src/routes/_Gumroad.svelte
@@ -1,11 +1,18 @@
+<script>
+	import Button from '../components/Button.svelte'
+</script>
+
 <style>
 	div {
-		font-family: 'norwester';
 		font-size: 1.3rem;
-		padding: 1.2rem;
+	}
+
+	div:only-child {
+		
 	}
 </style>
 
-<div id="gumroad-cta" class="gumroad-product-embed subscribe-link" data-gumroad-product-id="HardRoadOfHope">
-	<a id="order-link"  href="https://gumroad.com/l/HardRoadOfHope">Order Now</a>
+<div id="gumroad-cta" class="gumroad-product-embed" data-gumroad-product-id="HardRoadOfHope">
+	<!-- <a id="order-link"  href="https://gumroad.com/l/HardRoadOfHope">Order Now</a> -->
+	<Button id="order-link" href="https://gumroad.com/l/HardRoadOfHope">Order Now</Button>
 </div>
