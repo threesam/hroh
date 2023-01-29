1493eb01068693d67148acd55c4cc6c4814f98e3
threesam <salvatoredangelo@protonmail.com>
2023-01-29 13:29:12 -0500

    load p5 locally

--- web/src/components/CanvasP5.svelte
+++ web/src/components/CanvasP5.svelte
@@ -1,20 +1,20 @@
 <script>
-  export let id
-  import p5 from 'p5'
-  export let sketch
+  export let id;
+  import p5 from "./p5.min.js";
+  export let sketch;
 
-  import { onMount } from 'svelte'
+  import { onMount } from "svelte";
 
   onMount(() => {
-    let myp5 = new p5(sketch, id)
-  })
+    let myp5 = new p5(sketch, id);
+  });
 </script>
+
+<div {id} />
 
 <style>
   div {
     width: 100%;
     height: 100%;
   }
-  </style>
-
-<div {id} />
+</style>
